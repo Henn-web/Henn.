b1eb9a7f650e186fa4d6c1dcdef73dcaf395205b
Henn-web <tanhenryarianto@gmail.com>
2024-10-23 06:27:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -77,3 +77,56 @@ function showTools() {
     document.getElementById('techStack').style.display = 'none';
     document.getElementById('tools').style.display = 'grid';
 }
+
+// Typewriter//
+class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = '';
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 10);
+        this.type();
+        this.isDeleting = false;
+    }
+
+    type() {
+        const current = this.wordIndex % this.words.length;
+        const fullTxt = this.words[current];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.txtElement.innerHTML = `<span class="wrap">${this.txt}</span>`;
+
+        let typeSpeed = 200;
+
+        if (this.isDeleting) {
+            typeSpeed /= 2;
+        }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            typeSpeed = this.wait;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.wordIndex++;
+            typeSpeed = 500;
+        }
+
+        setTimeout(() => this.type(), typeSpeed);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    const txtElement = document.querySelector('.typewrite');
+    const words = JSON.parse(txtElement.getAttribute('data-type'));
+    const wait = txtElement.getAttribute('data-wait') || 3000;
+    new TypeWriter(txtElement, words, wait);
+}
+
